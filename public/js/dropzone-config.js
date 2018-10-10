c873b3344aa25cdeef3e976ab9ed897197dbbd65
TianRB <tianrb@gmail.com>
2018-10-10 10:27:02 -0500

    Update public/js/dropzone-config.js

--- public/js/dropzone-config.js
+++ public/js/dropzone-config.js
@@ -1,38 +1,50 @@
 Dropzone.autoDiscover = false;
+$(function() {
 
-var dzbg = new Dropzone('#img-bg', {
-	// url: $(this).attr.action,
-	thumbnailWidth: 80,
-	thumbnailHeight: 80,
-	parallelUploads: 20,
-	//previewsContainer: "#previews", // Define the container to display the previews
-	acceptedFiles:'image/*',
-	uploadMultiple: true,
-	parallelUploads: 4,
-	maxFiles: 1,
-	maxFilesize: 4,
-	dictRemoveFile: 'Remover Imagen',
-	dictFileTooBig: 'La imagen es mayor a 4MB',
-	timeout: 10000,
-});
-var dzfg = new Dropzone('#img-article', {
-	// url: $(this).attr.action,
-	thumbnailWidth: 80,
-	thumbnailHeight: 80,
-	parallelUploads: 20,
-	//previewsContainer: "#previews", // Define the container to display the previews
-	acceptedFiles:'image/*',
-	uploadMultiple: true,
-	parallelUploads: 4,
-	maxFiles: 4,
-	maxFilesize: 4,
-	dictRemoveFile: 'Remover Imagen',
-	dictFileTooBig: 'La imagen es mayor a 4MB',
-	timeout: 10000,
-});
+	var dzbg = new Dropzone('#img-bg', {
+		// url: $(this).attr.action,
+		thumbnailWidth: 80,
+		thumbnailHeight: 80,
+		parallelUploads: 20,
+		//previewsContainer: "#previews", // Define the container to display the previews
+		acceptedFiles:'image/*',
+		uploadMultiple: true,
+		parallelUploads: 4,
+		maxFiles: 1,
+		maxFilesize: 4,
+		dictRemoveFile: 'Remover Imagen',
+		dictFileTooBig: 'La imagen es mayor a 4MB',
+		timeout: 10000,
+	});
+	var dzfg = new Dropzone('#img-article', {
+		// url: $(this).attr.action,
+		thumbnailWidth: 80,
+		thumbnailHeight: 80,
+		parallelUploads: 20,
+		//previewsContainer: "#previews", // Define the container to display the previews
+		acceptedFiles:'image/*',
+		uploadMultiple: true,
+		parallelUploads: 4,
+		maxFiles: 4,
+		maxFilesize: 4,
+		dictRemoveFile: 'Remover Imagen',
+		dictFileTooBig: 'La imagen es mayor a 4MB',
+		timeout: 10000,
+	});
+
+	// Now that the DOM is fully loaded, create the dropzone, and setup the
+	// event listeners
+	dz.on("addedfile", function(file) {
+		/* Maybe display some more file information on your page */
+		
+	});
+})
+
+
+
+
 
 /*
-
 Dropzone.options.myDropzone = {
 //autoProcessQueue: false,
 thumbnailWidth: 80,
