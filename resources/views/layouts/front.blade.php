798df693df7ac19a385e4ef947add1cf7b14bea1
José Peña <Pepe@MacBook-Air-de-Jose.local>
2018-11-21 09:14:22 -0600

    enlaces redes

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -53,9 +53,9 @@
 			</div>
 			<div class="redes">
 				<p>Siguenos en nuestras redes sociales</p>
-				<span class="icon-facebook2"></span>
-				<span class="icon-instagram"></span>
-				<span class="icon-youtube"></span>
+				<a href="https://www.facebook.com/MRSeguridad/" target="_blank"><span class="icon-facebook2"></span></a>
+				<a href="https://www.instagram.com/explore/locations/156784164367692/mexico/veracruz-llave-veracruz-llave-mexico/mr-seguridad/?hl=es-la" target="_blank"><span class="icon-instagram"></span></a>
+				<a href="https://www.youtube.com/user/MRSeguridadVer" target="_blank"><span class="icon-youtube"></span></a>
 			</div>
 		</nav>
 		<div class="flechas-antes-despues">
